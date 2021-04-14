2d6e6cdbf821b894afdbb64adbae24e889a28a5d
codesangram <technicalstuffssangram@gmail.com>
2021-04-14 06:02:03 +0530

    sum cacluation is added

--- src/Sum.java
+++ src/Sum.java
@@ -4,5 +4,11 @@ public class Sum {
     {
         System.out.println("My calculator");
 
+        int a=3;
+        int b=4;
+        int c=a+b;
+        System.out.println("Sum =" + c);;
+
+
     }
 }
